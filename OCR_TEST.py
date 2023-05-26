cfa81e9744e7e36e50ea5a868b77c66d1b8de435
Bob Chrismansyah <kabangabcp20h@student.unhas.ac.id>
2023-05-26 11:05:57 +0800

    update

--- OCR_TEST.py
+++ OCR_TEST.py
@@ -1,3 +1,47 @@
+# import numpy as np
+# import cv2
+# import pickle
+# from keras.models import load_model
+#
+# width = 640
+# height = 480
+#
+# cap = cv2.VideoCapture(1)
+# cap.set(3, width)
+# cap.set(4, height)
+#
+# pickle_in = open("model_trained.h5", "rb")
+# model = pickle.load(pickle_in)
+# # model = load_model(pickle_in)  # Perbaikan: Menggunakan load_model untuk memuat model Keras
+#
+#
+# def preProcessing(img):
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     img = cv2.equalizeHist(img)
+#     img = img / 255
+#     return img
+#
+# while True:
+#     success, imgOriginal = cap.read()
+#     img = np.asarray(imgOriginal)
+#     img = cv2.resize(img, (32, 32))
+#     img = preProcessing(img)
+#     cv2.imshow("Preprocessed image", img)
+#     img = img.reshape(1, 32, 32, 1)
+#
+#     classIndex = model.predict_classes(img)  # Perbaikan: Menggunakan predict_classes untuk mendapatkan indeks kelas
+#     predictions = model.predict(img)
+#     probVal = np.amax(predictions)
+#
+#     if probVal > 0.65:
+#         cv2.putText(imgOriginal, str(classIndex), (50, 50), cv2.FONT_HERSHEY_COMPLEX,
+#                     1, (0, 0, 255), 1)
+#
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+
+
 import numpy as np
 import cv2
 import pickle
@@ -6,7 +50,9 @@ from keras.models import load_model
 width = 640
 height = 480
 
-cap = cv2.VideoCapture(1)
+
+
+cap = cv2.VideoCapture(0)
 cap.set(3, width)
 cap.set(4, height)
 
@@ -24,12 +70,12 @@ def preProcessing(img):
 while True:
     success, imgOriginal = cap.read()
     img = np.asarray(imgOriginal)
-    img = cv2.resize(img, (32, 32))
+    img = cv2.resize(img, (64, 64))
     img = preProcessing(img)
     cv2.imshow("Preprocessed image", img)
-    img = img.reshape(1, 32, 32, 1)
+    img = img.reshape(1, 64, 64, 1)
 
-    classIndex = model.predict_classes(img)  # Perbaikan: Menggunakan predict_classes untuk mendapatkan indeks kelas
+    classIndex = model.predict_step(img)  # Perbaikan: Menggunakan predict_classes untuk mendapatkan indeks kelas
     predictions = model.predict(img)
     probVal = np.amax(predictions)
 
@@ -39,3 +85,56 @@ while True:
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+# import numpy as np
+# import cv2
+# import pickle
+# from keras.models import load_model
+#
+# width = 640
+# height = 480
+#
+# cap = cv2.VideoCapture(0)
+# cap.set(3, width)
+# cap.set(4, height)
+#
+# #pickle_in = open("model_trained.h5", "rb")
+# # model = pickle.load(pickle_in)
+#
+# model = load_model("model_trained.p")
+# def preProcessing(img):
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     img = cv2.equalizeHist(img)
+#     img = img / 255
+#     return img
+#
+# while True:
+#     success, imgOriginal = cap.read()
+#     img = np.asarray(imgOriginal)
+#     img = cv2.resize(img, (32, 32))
+#     img = preProcessing(img)
+#     cv2.imshow("Preprocessed image", img)
+#     img = img.reshape(1, 32, 32, 1)
+#
+#     classIndex = model.predict_classes(img)
+#     predictions = model.predict(img)
+#     probVal = np.amax(predictions)
+#
+#     if probVal > 0.65:
+#         cv2.putText(imgOriginal, str(classIndex), (50, 50), cv2.FONT_HERSHEY_COMPLEX,
+#                     1, (0, 0, 255), 1)
+#
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+#
+# cap.release()
+# cv2.destroyAllWindows()
